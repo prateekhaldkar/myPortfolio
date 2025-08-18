da52300293d80cc067a206744cfeb14191b37150
Prateek Haldkar <137743180+prateekhaldkar@users.noreply.github.com>
2025-08-18 22:41:49 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -51,7 +51,7 @@ function Hero() {
             <a href="#">
               <button className="flex items-center bg-green-400 text-white hover:text-white border border-green-400 hover:bg-green-500 focus:ring-2 focus:outline-none focus:ring-green-400 font-medium rounded-lg text-base px-5 py-1 text-center mt-7 me-4 mb-2 dark:bg-green-400 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-400">
                   <img className="w-8 mr-2" src="/images/link.png" alt="" />
-                  GET MY RESUME kj
+                  GET MY RESUME
               </button>
             </a>
 
@@ -62,21 +62,21 @@ function Hero() {
 
           {/* Image section */}          
           <div className="lg:col-span-7 flex justify-center lg:justify-end mt-10 lg:mt-0">
-            {/* Large screens (tablet aur laptop) ke liye - Right side me align */}
-            {/* Debugging ke liye temporary */}
-            <img
-              className="hidden lg:block w-full max-w-lg md:max-w-xl lg:max-w-2xl object-contain ml-auto" 
-  src="/images/image1.png" 
-  alt="My Photo1"
-            />
+  {/* Large screens (tablet aur laptop) ke liye - Right side me align */}
+  <img
+    className="hidden lg:block w-full max-w-lg md:max-w-xl lg:max-w-2xl object-contain ml-auto"
+    src="/images/image1.png"
+    alt="My Photo1"
+  />
 
-            {/* Small screens ke liye - Center me align */}
-            <img
-              className="block lg:hidden w-full max-w-sm object-contain mx-auto" 
-  src="/images/image2.png" 
-  alt="My Photo2"
-            />
-          </div>
+  {/* Small screens ke liye - Center me align */}
+  <img
+    className="block lg:hidden w-full max-w-sm object-contain mx-auto"
+    src="/images/image2.png"
+    alt="My Photo2"
+  />
+</div>
+
 
 
 
