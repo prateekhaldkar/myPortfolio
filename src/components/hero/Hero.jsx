c79d295aedfe0b447c5f3f1e2d70aedaf56bfeb4
Prateek Haldkar <137743180+prateekhaldkar@users.noreply.github.com>
2025-08-19 14:23:02 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -463,7 +463,7 @@ function Hero() {
                 </div>
 
 
-                <div className="mt-5 flex justify-center">
+                <div className="mt-auto flex justify-center">
                   <a href="#" className="justify-center w-full inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     view
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
