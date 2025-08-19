5dd9b6ea322156a94865f9d99fd45fdb2b3164d3
Prateek Haldkar <137743180+prateekhaldkar@users.noreply.github.com>
2025-08-19 14:34:02 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -445,7 +445,7 @@ function Hero() {
               <a href="#">
                 <img className="rounded-t-lg" src="images/basic_calendar.png" alt="" />
               </a>
-              <div className="flex flex-col h-full p-5">
+              <div className="p-5">
                 <a href="#">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     Basic Calendar
@@ -461,13 +461,11 @@ function Hero() {
                   <img data-tooltip-target="tooltip-roberta" class="w-10 h-10 rounded-sm" src="/images/css.png" alt="Medium avatar" />
                   <img data-tooltip-target="tooltip-bonnie" class="w-10 h-10 rounded-sm" src="/images/javascript.png" alt="Medium avatar" />
                 </div>
-                <div className="flex-grow">
 
-                </div>
 
-                <div className="mt-auto flex justify-center">
+                <div className="mt-5 flex justify-center">
                   <a href="#" className="justify-center w-full inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    view
+                    view kar
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
