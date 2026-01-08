483388982c12f8b50f973e9e017cd4441b0907fb
Prateek Haldkar <137743180+prateekhaldkar@users.noreply.github.com>
2026-01-08 17:26:34 +0530

    ..

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,7 +26,7 @@ function navbar() {
                                 <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">About</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                                <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
                             </li>
                             <li>
                                 <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
