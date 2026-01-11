2dfc1184ad42fb6b5de5aa4c4bf41a1356424fe0
Prateek Haldkar <137743180+prateekhaldkar@users.noreply.github.com>
2026-01-11 16:42:40 +0530

    ..

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ function navbar() {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">About..</a>
                             </li>
                             <li>
                                 <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:text-green-100 dark:text-white dark:hover:bg-green-700 dark:hover:text-green-700 md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
